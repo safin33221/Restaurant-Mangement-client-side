627b9f67aacb78e6531c1dc395e550d9aedcec6e
safin33221 <saifn33221@gmail.com>
2024-12-21 23:50:20 +0600

    completed home page banner section

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,53 @@
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import slid1 from '../../assets/Banner/1.png'
+import slid2 from '../../assets/Banner/2.png'
+import slid3 from '../../assets/Banner/3.png'
+import slid4 from '../../assets/Banner/4.png'
+import slid5 from '../../assets/Banner/5.png'
+import slid6 from '../../assets/Banner/6.png'
+import slid7 from '../../assets/Banner/7.png'
+import slid8 from '../../assets/Banner/8.png'
+import slid9 from '../../assets/Banner/9.png'
+import slid10 from '../../assets/Banner/8.png'
+
+const Banner = () => {
+    return (
+        <div className='py-12'>
+            {/* <img src={slid1} alt="" /> */}
+            <Swiper
+                className='w-7/12 mx-auto'
+                spaceBetween={20}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+
+            >
+                <SwiperSlide><img src={slid1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid6} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid7} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid8} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid9} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={slid10} alt="" /></SwiperSlide>
+            </Swiper>
+
+        </div>
+    );
+};
+
+export default Banner;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -3,12 +3,17 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../Layouts/MainLayout';
 import Login from '../Pages/Login';
 import Register from '../Pages/Register';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
         children:[
+            {
+                path:'/',
+                element:<Home/>
+            },
             {
                 path:'/login',
                 element:<Login/>
