8a33be3ce231dd7233f7e9fd00aeae367b1a5e60
safin33221 <saifn33221@gmail.com>
2024-12-21 18:42:37 +0600

    completed footer section

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
 
 const MainLayout = () => {
     return (
@@ -15,6 +16,7 @@ const MainLayout = () => {
             <Outlet></Outlet>
 
             {/* Footer section  */}
+            <Footer></Footer>
         </div>
     );
 };
