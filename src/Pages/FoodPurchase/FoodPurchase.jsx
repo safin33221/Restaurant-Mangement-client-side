04cc98df7be3e58b7eb91c3a7b317284b7f7d08c
safin33221 <saifn33221@gmail.com>
2024-12-22 22:01:02 +0600

    completed to post food parchase data in DB

--- src/Pages/FoodPurchase/FoodPurchase.jsx
+++ src/Pages/FoodPurchase/FoodPurchase.jsx
@@ -1,22 +1,36 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { authContext } from "../../Provider/AuthProvider";
+import moment from "moment";
+import { toast } from "react-toastify";
 
 
 const FoodPurchase = () => {
     const { user } = useContext(authContext)
     const { id } = useParams()
     const [food, setFood] = useState({})
+    const navigate = useNavigate()
     useEffect(() => {
         axios.get(`http://localhost:8080/food/${id}`)
             .then(res => {
                 setFood(res.data)
             })
     }, [id])
-    console.log(food);
+
 
     const handleParchase = e => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const parchaseData = Object.fromEntries(formData.entries())
+        parchaseData.buying_Date = moment().format('L');
+        console.log(parchaseData);
+        axios.post('http://localhost:8080/food-parchase', parchaseData)
+            .then(res => {
+                console.log(res.data);
+                toast.success('Purchase successful! We’ll get your items ready soon.')
+                // navigate('/myOrders')
+            })
 
     }
 
@@ -26,43 +40,45 @@ const FoodPurchase = () => {
                 <h2 className="text-2xl font-semibold text-center text-green-500">Food Purchase</h2>
 
                 <div className="grid grid-cols-12 gap-4">
-                    <div className="col-span-12 md:col-span-6 flex flex-col">
+                    <div className="col-span-12  flex flex-col">
                         <label className="text-lg text-gray-700">Food Name</label>
                         <input
                             type="text"
                             name="foodName"
-                            defaultValue={food.foodName}
+                            value={food.foodName}
+                            readOnly
                             required
                             className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-100"
                         />
                     </div>
 
+                    <div className="col-span-12 md:col-span-6 flex flex-col">
+                        <label className="text-lg text-gray-700">Price</label>
+                        <input
 
+                            name="price"
+                            value={food.price}
+                            readOnly
 
-                    <div className="col-span-12 md:col-span-3 flex flex-col">
+                            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-100"
+                        />
+                    </div>
+
+                    <div className="col-span-12 md:col-span-6 flex flex-col">
                         <label className="text-lg text-gray-700">Quantity</label>
                         <input
-                            type="number"
+
                             name="quantity"
-                            defaultValue={food.quantity}
+                            value={food.quantity}
+                            readOnly
                             required
-                            min="1"
+
                             className="p-3 border border-gray-300 rounded-lg focus:outline-none  focus:ring-2 focus:ring-green-100"
                         />
                     </div>
 
-                    <div className="col-span-12 md:col-span-3 flex flex-col">
-                        <label className="text-lg text-gray-700">Price</label>
-                        <input
-                            type="number"
-                            name="price"
-                            defaultValue={food.price}
-                            required
-                            step="0.01"
-                            min="0"
-                            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-100"
-                        />
-                    </div>
+
+
 
 
                     <div className="flex flex-col col-span-12 md:col-span-6 space-y-2">
@@ -71,7 +87,7 @@ const FoodPurchase = () => {
 
                             <input
                                 type="email"
-                                name='email'
+                                name='name'
                                 value={user?.displayName || 'not found'}
                                 readOnly
                                 className="p-3 border border-gray-300 rounded-lg focus:outline-none w-full focus:ring-2 focus:ring-green-100"
