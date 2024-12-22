d842df13ca26180633bee2fe7c9ffa8b57ec0806
safin33221 <saifn33221@gmail.com>
2024-12-22 16:05:18 +0600

    completed navbar profile funtionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -40,28 +40,31 @@ const Navbar = () => {
                     }
 
                 </div>
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                {
+                    user ? <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
                         </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><NavLink to='/myFoods'>My Foods</NavLink></li>
+                            <li><NavLink to='/addFood'>Add food</NavLink></li>
+                            <li><NavLink to='/myOrders'>My Orders</NavLink></li>
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><NavLink to='/myFoods'>My Foods</NavLink></li>
-                        <li><NavLink to='/addFood'>Add food</NavLink></li>
-                        <li><NavLink to='/myOrders'>My Orders</NavLink></li>
-                    </ul>
-                </div>
+                        : null
+                }
             </div>
         </div>
     );
