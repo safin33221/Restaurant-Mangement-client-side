4222a373889d3123aa422c045585bef760b88cd2
safin33221 <saifn33221@gmail.com>
2024-12-25 22:00:18 +0600

    showed a toast when auth/invalid=credential

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -23,6 +23,21 @@ const Login = () => {
                 toast.success(' Welcome back! Login successful.')
                 navigate('/')
             })
+            .catch(error => {
+
+                if (error.code === "auth/invalid-credential") {
+                    return toast.error('Oops! The email or password you entered is incorrect. Please try again.', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        theme: "light",
+                       
+                    })
+                }
+            })
     }
     const handleLoginWithGoogle = () => {
         signInWithGoogle()
@@ -47,7 +62,7 @@ const Login = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 16 16"
                                 fill="currentColor"
-                               className="h-4 w-4 opacity-70">
+                                className="h-4 w-4 opacity-70">
                                 <path
                                     d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                                 <path
